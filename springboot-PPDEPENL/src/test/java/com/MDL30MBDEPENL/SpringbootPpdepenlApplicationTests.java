bfed29ff194d1f9db4967f25331359ddb711b202
izanmaska <ethan.ca@hotmail.com>
2023-09-19 18:22:13 +0200

    Initial commit

---
+++ springboot-PPDEPENL/src/test/java/com/MDL30MBDEPENL/SpringbootPpdepenlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MDL30MBDEPENL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPpdepenlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
